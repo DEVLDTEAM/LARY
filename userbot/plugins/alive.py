29f4ec3a5ea727ff39411b64af7642104c628b50
Angly <83790701+THHBOB@users.noreply.github.com>
2021-06-08 17:24:39 +0300

    LaRy

--- userbot/plugins/alive.py
+++ userbot/plugins/alive.py
@@ -40,7 +40,7 @@ async def amireallyalive(event):
         cat_caption = f"**{CUSTOM_ALIVE_TEXT}**\n\n"
         cat_caption += f"**{EMOJI} Database :** `{check_sgnirts}`\n"
         cat_caption += f"**{EMOJI} Telethon version :** `{version.__version__}\n`"
-        cat_caption += f"**{EMOJI} Catuserbot Version :** `{catversion}`\n"
+        cat_caption += f"**{EMOJI} LaRy Version :** `{catversion}`\n"
         cat_caption += f"**{EMOJI} Python Version :** `{python_version()}\n`"
         cat_caption += f"**{EMOJI} Uptime :** `{uptime}\n`"
         cat_caption += f"**{EMOJI} Master:** {mention}\n"
